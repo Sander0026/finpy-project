6151ccbdbe2abd5b98158167977e13aac02ca032
alexsander ribeiro <sander-vl@hotmail.com>
2025-06-18 09:23:56 -0300

    feat: adiciona estrutura do BD e script de conexão com MySQL

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask
+import mysql.connector
+from mysql.connector import Error
 
-# Cria uma instância da aplicação Flask
 app = Flask(__name__)
 
-# Define a rota principal da aplicação
+# Função para criar a conexão com o banco de dados
+def create_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',       # Ou o IP do seu servidor de BD
+            user='root',            # Seu usuário do MySQL
+            password='Root123456*',    # Sua senha do MySQL
+            database='finpy_db'
+        )
+        print("Conexão com o MySQL bem-sucedida")
+        return connection
+    except Error as e:
+        print(f"O erro '{e}' ocorreu")
+        return None
+
 @app.route('/')
 def home():
-    return "Olá, FinPy! Meu servidor está no ar."
+    conn = create_db_connection()
+    if conn is not None:
+        # Se a conexão foi bem-sucedida, podemos fechá-la e retornar uma mensagem
+        conn.close()
+        return "Conexão com o banco de dados estabelecida com sucesso!"
+    else:
+        # Se a conexão falhou, retornamos uma mensagem de erro
+        return "Falha ao conectar ao banco de dados."
 
-# Permite que o servidor seja executado quando o script é chamado diretamente
 if __name__ == '__main__':
     app.run(debug=True)
